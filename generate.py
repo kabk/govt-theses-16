c4584984e6d966f08605229e0cb3f235c436642d
Eric Schrijver <eric@ericschrijver.nl>
2015-11-26 09:55:05 +0100

    First list

---
+++ generate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from urllib2 import urlopen
+from codecs import open
+import json
+import re
+
+res = urlopen("https://api.github.com/search/repositories?q=govt-theses-16+in:name+user:kabk")
+
+repos = json.load(res)
+
+thesis_template = """
+<div class="preview">
+    <h2><a href="{url}">{slug}</a></h2>
+</div>
+"""
+
+thesis_links = ""
+for thesis in repos['items']:
+    thesis_links += thesis_template.format(
+                                  url='http//kabk.github.io/%s/' % thesis['name'],
+                                  slug=thesis['name'])
+
+print thesis_links
