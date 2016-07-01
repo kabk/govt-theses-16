8e5383bd3cd26cf062fa66f07044d2470d217007
martijndeheer <martijndeheer@gmail.com>
2016-07-01 14:43:44 +0200

    New Index design
    concept, design && code by Rafael, Luc & Martijn

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,144 @@
+// $(window).load(function(){
+
+//   var $gal   = $("article #wrapper"),
+//     galW   = $gal.outerHeight(true),
+//     galSW  = $gal[0].scrollHeight,
+//     wDiff  = (galSW/galW)-1,  // Heights difference ratio
+//     mPadd  = 60,  // Mousemove Padding
+//     damp   = 50,  // Mousemove response softness
+//     mX     = 0,   // Real mouse position
+//     mX2    = 0,   // Modified mouse position
+//     posX   = 0,
+//     mmAA   = galW-(mPadd*2), // The mousemove available area
+//     mmAAr  = (galW/mmAA);    // get available mousemove fidderence ratio
+
+//   $gal.mousemove(function(e) {
+//     mX = e.pageY - $(this).parent().offset().top - this.offsetLeft;
+//     mX2 = Math.min( Math.max(0, mX-mPadd), mmAA ) * mmAAr;
+//   });
+
+//   setInterval(function(){
+//     posX += (mX2 - posX) / damp; // zeno's paradox equation "catching delay" 
+//     $gal.scrollTop(posX*wDiff);
+//   }, 10);
+// });
+
+//
+// when hover over sentence
+$('#mainCol a').mouseenter(function(event){
+
+  var name = $(this).attr('data-name');
+
+  $("#leftCol .preview h3").each(function(){
+    $(this).css('color','#000').removeClass('hovereditem');
+  })
+
+  $("#mainCol a").each(function(){
+    $(this).css({'color' : '#000', 'text-decoration' : 'none'});
+  });
+
+  $(this).css({'color' : '#00f', 'text-decoration' : 'underline'});
+
+  // $('#mainCol a').each(function(i,a){
+
+  //   return this.innerHTML = this.innerText.replace('g','<span class="no-underline">g</span>');
+
+  // });
+
+  $("#leftCol .preview h3:contains('" + name + "')").parent().find('h3').css('color', '#00f').addClass('hovereditem');
+
+});
+
+//
+// when hover over person names
+$('#leftCol .preview h3').mouseenter(function(){
+
+  var name = $(this).text();
+  console.log(name);
+
+  $("#leftCol .preview h3").each(function(){
+    $(this).css('color','#000').removeClass('hovereditem');
+  });
+
+  $("#mainCol a").each(function(){
+    $(this).css({'color' : '#000', 'text-decoration' : 'none'});
+  });
+
+  $(this).css('color', '#00f').addClass('hovereditem');
+
+  var getThisSentence = $("#mainCol a[data-name='" + name + "']");
+  getThisSentence.css({'color' : '#00f', 'text-decoration' : 'underline'});
+
+  // if(getThisSentence.length > 0){
+  //   var getThisSentenceOffSet = getThisSentence.offset().top;
+  //   // var getThisSentenceTOP = getThisSentenceOffSet.top;
+
+  //   var getThisSentenceScrollTOP = $('#mainCol').scrollTop();
+
+  //   console.log(getThisSentenceScrollTOP);
+
+  //   $('#mainCol #wrapper').animate({scrollTop : getThisSentenceOffSet}, 1000);
+
+  // }
+});
+
+// $('#leftCol').mouseleave(function(){
+
+//     $('#mainCol #wrapper').scrollTop(0);
+
+// });
+
+//
+// when click on sentences
+$('#mainCol a').click(function(event){
+
+  event.preventDefault();
+
+  var name = $(this).attr('data-name');
+
+  $("#leftCol .preview h3").each(function(){
+    $(this).removeClass('currentyleSelected');
+  });
+
+  $("#leftCol .preview figure").each(function(){
+    $(this).css('display','none');
+  });
+
+  $("#leftCol .preview h2").each(function(){
+    $(this).css('display','none');
+  });
+
+  $("#leftCol .preview p").each(function(){
+    $(this).css('display','none');
+  });
+
+  $("#leftCol .preview h3:contains('" + name + "')").addClass('currentyleSelected').parent().find('h2').addClass('active').toggle();
+  $("#leftCol .preview h3:contains('" + name + "')").addClass('currentyleSelected').parent().find('p').addClass('active').toggle();
+});
+
+//
+// when click on person names
+$('#leftCol .preview h3').click(function(){
+
+  $("#leftCol .preview h3").each(function(){
+    $(this).removeClass('currentyleSelected');
+  });
+
+  $("#leftCol .preview figure").each(function(){
+    $(this).css('display','none');
+  });
+
+  $("#leftCol .preview h2").each(function(){
+    $(this).css('display','none');
+  });
+
+  $("#leftCol .preview p").each(function(){
+    $(this).css('display','none');
+  });
+
+  $(this).addClass('currentyleSelected').parent().find('figure').toggle();
+});
+
 /** This script does nothing, except printing a small message to the console.
 *   The console is a part of the browser where you can find feedback on how your scripts are doing
 *   You can find it when you choose ‘inspect element’ and choose the ‘console’ tab
